d0b4b712b4865a50232880407c3c61aef5a15136
Rifat Ahammed <reardj007@gmail.com>
2022-07-21 16:34:43 +0600

    Join Ssn, Onfind ssn, ssn search Added

--- MenuSystem/Source/MenuSystem/MenuSystemCharacter.h
+++ MenuSystem/Source/MenuSystem/MenuSystemCharacter.h
@@ -73,10 +73,16 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void CreateGameSession();
 
+	UFUNCTION(BlueprintCallable)
+	void JoinGameSession();
+
 	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
 
 private:
 
 	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	TSharedPtr<FOnlineSessionSearch> SessionSearch;
 };
 
